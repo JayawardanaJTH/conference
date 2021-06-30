107ee7c51bd10575b472336abd8f7a697f9c09ba
Jayawardana JTH <tiran2323@gmail.com>
2021-06-30 21:06:14 +0530

    Modify routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
+import AdminPanel from '../components/pages/adminPanel/AdminPanel';
 import Documents from '../components/pages/documents/Documents';
 import EditPage from '../components/pages/editPage/EditPage';
-import ViewGuest from '../components/pages/guest/ViewGuest';
 import Home from '../components/pages/home/Home';
 import LoginRegistration from '../components/pages/login-Registration/LoginRegistration';
 import Payment from '../components/pages/payment/Payment';
@@ -16,11 +16,11 @@ const Routes = () => {
             <Route exact path="/" component={Home} />
             <Route exact path="/signin-signup" component={LoginRegistration} />
             <Route exact path="/documents" component={Documents} />
-            <Route exact path="/guests" component={ViewGuest} />
             <Route exact path="/payment" component={Payment} />
             <Route exact path="/workshop" component={WorkShop} />
             <Route exact path="/research" component={Research} />
             <Route exact path="/edit" component={EditPage} />
+            <Route exact path="/admin" component={AdminPanel} />
         </Switch>
      );
 }
